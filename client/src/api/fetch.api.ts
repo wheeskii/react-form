17f1b8e365ac81b87fbcf621509130e15a347130
wheeskii <delacruz.ellezir@gmail.com>
2025-11-04 14:59:05 +0800

    added axios instance for updating and getting users by using id

--- client/src/api/fetch.api.ts
+++ client/src/api/fetch.api.ts
@@ -1,7 +1,17 @@
-import type { UserType } from "../validator/user.validator";
+import type { UserType, UserUpdateType } from "../validator/user.validator";
 import { axiosInstance } from "./axios.config";
 
-export const getAllUsers = async ():Promise<UserType[]> => {
-    const {data} = await axiosInstance.get("/users");
+export const getAllUsers = async (): Promise<UserType[]> => {
+    const { data } = await axiosInstance.get("/users");
     return data.users;
+};
+
+export const getUserByID = async (id: any): Promise<UserType> =>  {
+    const { data } = await axiosInstance.get(`/users/${id}`)
+    return data.user;
+};
+
+export const updateUserByID = async (id: any, updatedData: UserUpdateType): Promise<UserType> =>  {
+    const { data } = await axiosInstance.patch(`/users/${id}`)
+    return data.user;
 };
