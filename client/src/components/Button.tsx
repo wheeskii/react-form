28d943cd6e7f62473aebb9657b33fff7669198f6
wheeskii <delacruz.ellezir@gmail.com>
2025-11-04 09:37:39 +0800

    updated buttons

--- client/src/components/Button.tsx
+++ client/src/components/Button.tsx
@@ -1,8 +1,10 @@
 import { Button, type ButtonProps } from '@mui/material';
 
-export function OutlinedButton(props: ButtonProps) {
-    return <Button variant='contained' {...props}>{props.children}</Button>
+export function ContainedButton(props: ButtonProps) {
+    return <Button variant='contained' disableElevation {...props}>{props.children}</Button>
 }
+
 export function LogoutButton(props: ButtonProps) {
     return <Button sx={{ color: 'white'}} {...props}>{props.children}</Button>
 }
+
