abaa770c9928a2160bfbd7a7f1286c228d9d5274
wheeskii <delacruz.ellezir@gmail.com>
2025-11-24 08:48:47 +0800

    removed saving refresh token in storage

--- client/src/api/authentication.api.ts
+++ client/src/api/authentication.api.ts
@@ -3,6 +3,6 @@ import { axiosInstance } from "./axios.config";
 export const loginUser = async (email: string) => {
   const { data } = await axiosInstance.post("/signin", { email });
   localStorage.setItem("accessToken", data.accessToken);
-  localStorage.setItem("refreshToken", data.refreshToken);
+  // localStorage.setItem("refreshToken", data.refreshToken);
   return data.user;
 };
